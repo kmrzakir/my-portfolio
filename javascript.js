4baf321bca97d2e8f441053cabd4cb346b2eca17
zakir <zakirmustafa444@gmaol.com>
2024-11-10 22:59:07 +0530

    made responcive

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function toggleMenu(){
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('show');
+}
+
+
+// FOR TEXT ANIMATION
+var typed = new Typed('#element', {
+    strings: ['Web developer', 'Android App Developer', "content creator", "And also a Gamer..."],
+    typeSpeed: 50,
+});
+var typed = new Typed('#element2', {
+    strings: ['Web developer', 'Android App Developer', "content creator", "And also a Gamer..."],
+    typeSpeed: 50,
+});
